f5b529ba21f28dc1c197eb9a0aa5e4567fdce551
netedsoftech <naman@netedsoftech.com>
2024-02-12 13:29:44 +0530

    first commit

---
+++ src/appWrite/auth.js
@@ -0,0 +1,75 @@
+import config from "../confi/config";
+import { Client, Account, ID  } from "appwrite";
+
+export class AuthService {
+     client = new Client();
+     account;
+
+     constructor(){
+        this.client
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client)
+     }
+
+// Create user account start
+async createAccount({email, password, name}) {
+    try {
+     const userAccount =  await this.account.create(ID.unique(), email, password, name)
+       if(userAccount) {
+        // call another method
+           return this.login({email, password})
+       }
+       else{
+        return userAccount
+       }
+    } catch (error) {
+        throw error;
+    }
+ }
+// Create user account end
+
+// Login user account start
+
+     async login({email, password}) {
+        try {
+          return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+     }
+// Login user account end
+
+
+// Current user account start
+
+async getCurrentUser(){
+    try{
+        return await this.account.get();
+    }catch (error) {
+        console.log("Appwrite Service :: getCurrentUser :: error", error)
+    }
+    return null;
+}
+
+// Current user account end
+
+
+// Logout user account start
+
+async logout(){
+    try{
+       return await this.account.deleteSessions();
+    }catch (error){
+        console.log("Appwrite Service :: logout :: error", error)
+    }
+}
+
+// Logout user account end
+
+}
+
+const authService = new AuthService();
+
+export default authService
+
